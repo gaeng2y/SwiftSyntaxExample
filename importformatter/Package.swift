3b9fe7901cb1e20021ce011dd0145b5f02fda820
gaeng2y <gaeng2y@gmail.com>
2024-06-27 15:17:09 +0900

    Section 2
    Configure Your Package
    Configure your package and get started with a command line application.

--- importformatter/Package.swift
+++ importformatter/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "importformatter",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .executable(
@@ -12,11 +16,19 @@ let package = Package(
             targets: ["importformatter"]
         )
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/apple/swift-syntax.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "importformatter"
+            name: "importformatter",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftParser", package: "swift-syntax"),
+            ]
         ),
         .testTarget(
             name: "importformatterTests",
